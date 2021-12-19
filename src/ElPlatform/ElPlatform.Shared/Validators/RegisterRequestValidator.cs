181849a47e6722c9c8ab0027cb5325805d79a19c
Ahmed Yousef <Ahmed.Youssef.abdalla@gmail.com>
2021-12-19 20:08:26 +0400

    Add Fluent validation and validate register model

---
+++ src/ElPlatform/ElPlatform.Shared/Validators/RegisterRequestValidator.cs
@@ -0,0 +1,30 @@
+﻿using ElPlatform.Shared.Models;
+using FluentValidation;
+namespace ElPlatform.Shared.Validators
+{
+    public class RegisterRequestValidator : AbstractValidator<RegisterRequest>
+    {
+        public RegisterRequestValidator()
+        {
+            RuleFor(p => p.Email)
+                .NotEmpty().
+                WithMessage("Email Is Requierd")
+                .EmailAddress().
+                WithMessage("Not a valid email address");
+            RuleFor(p => p.FirstName)
+               .NotEmpty().
+               WithMessage("First Name Is Requierd");
+            RuleFor(p => p.LastName)
+               .NotEmpty().
+               WithMessage("Last Name Is Requierd");
+            RuleFor(p => p.Password)
+               .NotEmpty().
+               WithMessage("Password Is Requierd")
+               .MinimumLength(6)
+               .WithMessage("Password Can't be less than 6 char");
+            RuleFor(p => p.ConfirmPassword)
+              .Equal(p => p.Password)
+              .WithMessage("Confirm Password dosen't match password");
+        }
+    }
+}
